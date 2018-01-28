fcd56e7fa9d71cdb33cfe61b5824e16b3bcd4fcc
coreyfedde <coreysfedde@gmail.com>
2018-01-27 19:20:32 -0500

    Added styling to layout and functionality for portfolio items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ class Footer extends Component {
     return (
       <div className='footer-parent'>
         <div className='footer-links'>
-          <a href="#">Instagram</a>
-          <a href="#">LinkedIn</a>
-          <a href="#">Github</a>
+          <a href="#"><img src="tam-site/src/assets/images/github.png" height='42' width='42' /></a>
+          <a href="#"><img src="../assets/images/linkedin.png" /></a>
+          <a href="#"><img src="github.png" /></a>
         </div>
         <div className='footer-name'>
           <h5>Tamara Long 2018</h5>
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,15 +6,45 @@ class About extends Component {
     return (
       <div className="about-grid">
         <div className="self-description">
-          <div className="text">Description</div>
+          <div className="description-content">
+            <div className="placeholder-image">IMAGE</div>
+            <div className="text">
+              <p>Raised in the heart of it all, I am a Columbus, Ohio native, now
+              living in Austin, Texas. I am currently working as a Senior User
+              Experience Associate at athenahealth. I love what I do.</p>
+              <p>While interaction and user flow design are some of my core strengths,
+              you will often find me participating in a variety of cross-disciplinary
+              activities including user research, stakeholder interviews, information
+              architecture, content strategy, front end development, and even some
+              audio production. It was my career goal, once upon a time, to be a
+              record producer. Making music is still something I love to do, but is
+              much more of a hobby at this point.</p>
+              <p>When it comes to work, I want to be involved in projects that I
+              find challenging with people that inspire me. Thanks for stopping by.
+              </p>
+              <p>— Zach</p>
+            </div>
+          </div>
         </div>
         <div className="interests">
-          <div className="creating">Creating</div>
-          <div className="reading">Reading</div>
-          <div className="listening">Listening</div>
+          <div className="creating">
+            <h3>Tweeting...</h3>
+            <div>Placeholder</div>
+          </div>
+          <div className="reading">
+            <h3>Reading...</h3>
+            <div>Placeholder</div>
+          </div>
+          <div className="listening">
+            <h3>Listening...</h3>
+            <div>Placeholder</div>
+          </div>
         </div>
         <div className="contact">
-          <h1>Contact Email</h1>
+          <div className="contact-content">
+            <h3>Want to work together? Email me:</h3>
+            <h1>email@gmail.com</h1>
+          </div>
         </div>
       </div>
     );
