c6cde0a63c76422fe92529b29ef74354966b70ec
coreyfedde <coreysfedde@gmail.com>
2018-03-05 20:42:52 -0500

    Removed placeholder text

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,20 +9,7 @@ class About extends Component {
           <div className="description-content">
             <div className="placeholder-image">IMAGE</div>
             <div className="text">
-              <p>Raised in the heart of it all, I am a Columbus, Ohio native, now
-              living in Austin, Texas. I am currently working as a Senior User
-              Experience Associate at athenahealth. I love what I do.</p>
-              <p>While interaction and user flow design are some of my core strengths,
-              you will often find me participating in a variety of cross-disciplinary
-              activities including user research, stakeholder interviews, information
-              architecture, content strategy, front end development, and even some
-              audio production. It was my career goal, once upon a time, to be a
-              record producer. Making music is still something I love to do, but is
-              much more of a hobby at this point.</p>
-              <p>When it comes to work, I want to be involved in projects that I
-              find challenging with people that inspire me. Thanks for stopping by.
-              </p>
-              <p>— Zach</p>
+              <p>Placeholder text</p>
             </div>
           </div>
         </div>
